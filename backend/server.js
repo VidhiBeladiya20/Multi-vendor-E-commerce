76ce0cd908d09693d13e5867670efe7958624407
ManaliDhanani <manalidhanani105@gmail.com>
2024-02-17 10:57:37 -0500

    make change

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,6 @@ app.use("/user",user);
 
 connectDb().then(()=>{
     app.listen("5000",()=>{
-        console.log("Running at port on palak 5000");
+        console.log("Running at port number 5000");
     })
 })
