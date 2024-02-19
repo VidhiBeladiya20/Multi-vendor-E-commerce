7f78bdeee9b69c219fae1fbf5eb7ecf88fe65939
Vishva Balar <vishubalar29@gmail.com>
2024-02-19 23:54:25 +0530

    Add shopping page.

---
+++ frontend/public/assets/js/test.js
@@ -0,0 +1,19 @@
+/*------------------
+        Accordin Active
+    --------------------*/
+    $('.collapse').on('shown.bs.collapse', function () {
+        $(this).prev().addClass('active');
+    });
+
+    $('.collapse').on('hidden.bs.collapse', function () {
+        $(this).prev().removeClass('active');
+    });
+
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import {Home} from "./pages/Home";
 import {About} from "./pages/About";
 import {SignUp} from "./pages/SignUp";
 import { Login } from "./pages/Login";
+import { Shop } from "./pages/Shop" ;
+
 export const App=()=>{
   return (
     <>
@@ -11,6 +13,7 @@ export const App=()=>{
       {/* <Navbar/> */}
           <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/shop" element={<Shop/>}/>
             <Route path="/about" element={<About/>}/>
             <Route path="/signup" element={<SignUp/>}/>
             <Route path="/login" element={<Login/>}/>
